4110d37e39327b080669bcd120516671a0cabdbc
Sahil Agarwal <sahil.agarwal@xoom.com>
2016-11-26 22:49:43 -0800

    Added a getTask method / modified TaskInstance schema

--- TaskHero/Step.swift
+++ TaskHero/Step.swift
@@ -13,5 +13,66 @@ class Step: NSObject {
     var details: String?
     var state: String?
     var assignees: [User]?
-    var signoffs: [User]?
+    var signoff: User?
+    var completedBy: User?
+    var completedAt: TimeInterval?
+    
+    override init() {
+        super.init()
+    }
+    
+    init(stepDictionary: [String: AnyObject]?) {
+        super.init()
+        
+        self.name = stepDictionary?["name"] as! String?
+        self.getAssignees(assignees: stepDictionary?["assignees"] as! [String])
+        
+        if let details = stepDictionary?["details"] as? String?  {
+            self.details = details
+        }
+        if let state = stepDictionary?["state"] as? String?  {
+            self.state = state
+        }
+        if let completedAt = stepDictionary?["completed_at"] {
+            self.completedAt = (completedAt as! TimeInterval)
+        }
+        if let completedBy = stepDictionary?["completed_by"] {
+            getCompletedBy(completedBy: (completedBy as! String))
+        }
+        if let signoff = stepDictionary?["signoff"]  {
+            getSignoff(signoff: (signoff as! String))
+        }
+    }
+    
+    private func getAssignees(assignees: [String]) {
+        for assignee in assignees {
+            ParseClient.sharedInstance.getUser(userId: assignee, success: { (user) -> () in
+                if self.assignees == nil {
+                    self.assignees = [user]
+                } else {
+                    self.assignees?.append(user)
+                }
+            }, failure: { error in
+                NSLog("Error getting users, error: \(error)")
+            })
+        }
+    }
+    
+    private func getSignoff(signoff: String) {
+        ParseClient.sharedInstance.getUser(userId: signoff, success: { (user) -> () in
+            self.signoff? = user
+        }, failure: { error in
+            NSLog("Error getting user, error: \(error)")
+        })
+    }
+    
+    private func getCompletedBy(completedBy: String) {
+        ParseClient.sharedInstance.getUser(userId: completedBy, success: { (user) -> () in
+            self.signoff? = user
+        }, failure: { error in
+            NSLog("Error getting user, error: \(error)")
+        })
+    }
+    
+    
 }
