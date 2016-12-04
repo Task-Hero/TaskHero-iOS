a583d0c39805d6c264198e7088d850f1cfe03264
aki shinagawa <akishinagawa@hotmail.com>
2016-12-04 01:17:06 -0800

    changed buttom bar color changed NavBar tint color

--- TaskHero/AppColors.swift
+++ TaskHero/AppColors.swift
@@ -13,12 +13,13 @@ class AppColors: NSObject {
     static let appBlue = UIColor.init(red: 0/255, green: 142/255, blue: 218/255, alpha: 1)
     static let appGreen = UIColor.init(red: 0/255, green: 205/255, blue: 52/255, alpha: 1)
     static let appGrey = UIColor.init(red: 249/255, green: 249/255, blue: 249/255, alpha: 1)
+    static let appMidGrey = UIColor.init(red: 127/255, green: 127/255, blue: 127/255, alpha: 1)
     static let appWhite = UIColor.white
     static let appBlack = UIColor.black
     
     static func loadNavigationBarColors(navigationController: UINavigationController) {
         navigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: AppColors.appWhite]
         navigationController.navigationBar.barTintColor = AppColors.appRed
-        navigationController.navigationBar.tintColor = AppColors.appBlack
+        navigationController.navigationBar.tintColor = AppColors.appWhite
     }
 }
