641cc9a890278e693f59944a1b5c8e0cb9fbf788
Sahil Agarwal <sahil.agarwal@xoom.com>
2016-12-06 23:17:13 -0800

    WIP

---
+++ TaskHero/SimpleDateFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleDateFormatter.swift
+//  TaskHero
+//
+//  Created by Sahil Agarwal on 12/6/16.
+//  Copyright © 2016 Task Hero. All rights reserved.
+//
+
+import UIKit
+
+class SimpleDateFormatter: NSObject {
+
+}
